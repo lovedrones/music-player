46ed8bb7791282fe937ba6a84d5a536bd0169f4e
Juan Lo <lovedrones@gmail.com>
2022-09-14 07:09:35 -0400

    playback function

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
 
+// Music 
+const songs = [
+    {
+        name: 'jacinto-1',
+        displayName: 'Electric Chill Machine',
+        artist: 'Jacinto Design',
 
+    },
+    {
+        name: 'jacinto-2',
+        displayName: 'Seven Nation Army (Remix)',
+        artist: 'Jacinto Design',
+    },
+    {
+        name: 'jacinto-3',
+        displayName: 'Goodnight, Disco Queen',
+        artist: 'Jacinto Design',
+    },
+    {
+        name: 'metric-1',
+        displayName: 'Front Row (remix)',
+        artist: 'Metric/Jacinto Design',
+    },
+];
 //Check if Playing 
 let isPlaying = false;
 
@@ -25,3 +51,32 @@ function pauseSong() {
 
 //Play or Pause Event Listener 
 playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+
+// Update DOM
+function loadSong(song) {
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`;
+    image.src = `img/${song.name}.jpg`;
+}
+
+
+// Current Song
+
+let songIndex = 0;
+
+// Previous Song 
+function prevSong() {
+    songIndex--;
+    console.log(songIndex);
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// On Load - Select First Song 
+loadSong(songs[songIndex]);
+
+// Event Listeners
+
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
